3ed350c0c7ec7c2eb0e6ad1c315a7795973fdf56
Jamie Canning <theabysmalkraken@gmail.com>
2023-10-01 12:14:11 +0100

    Add CatanTile model

---
+++ Catan/Catan.Domain/CatanTile.cs
@@ -0,0 +1,35 @@
+﻿namespace Catan.Domain
+{
+    internal sealed class CatanTile
+    {
+        private CatanResourceType type;
+        private int activationNumber;
+
+        public CatanTile(CatanResourceType type, int activationNumber)
+        {
+            this.type = type;
+            this.activationNumber = activationNumber;
+        }
+
+        public CatanResourceType GetTileType()
+        {
+            return type;
+        }
+
+        public int GetActivationNumber()
+        {
+            return activationNumber;
+        }
+    }
+
+    public enum CatanResourceType
+    {
+        Unknown = 0,
+        Wood = 1,
+        Brick = 2,
+        Sheep = 3,
+        Wheat = 4,
+        Ore = 5,
+        Desert = 6
+    }
+}
