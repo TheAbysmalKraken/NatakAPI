ebe8aa5c71588cf18ea64b95dfa6169e6ae103d8
Jamie Canning <theabysmalkraken@gmail.com>
2023-10-01 12:26:00 +0100

    Add CatanBuilding model

---
+++ Catan/Catan.Domain/CatanBuilding.cs
@@ -0,0 +1,31 @@
+﻿namespace Catan.Domain
+{
+    internal sealed class CatanBuilding
+    {
+        private CatanPlayerColour colour;
+        private CatanBuildingType type;
+
+        public CatanBuilding(CatanPlayerColour colour, CatanBuildingType type)
+        {
+            this.colour = colour;
+            this.type = type;
+        }
+    }
+
+    public enum CatanPlayerColour
+    {
+        None = 0,
+        Red = 1,
+        Blue = 2,
+        Green = 3,
+        Yellow = 4
+    }
+
+    public enum CatanBuildingType
+    {
+        None = 0,
+        Road = 1,
+        Settlement = 2,
+        City = 3
+    }
+}
