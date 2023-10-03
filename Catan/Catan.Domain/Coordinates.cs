fb726f76ea443eac3d64ca172f4856b32a35819a
Jamie Canning <theabysmalkraken@gmail.com>
2023-10-03 20:37:44 +0100

    Add coordinate maths

--- Catan/Catan.Domain/Coordinates.cs
+++ Catan/Catan.Domain/Coordinates.cs
@@ -1,4 +1,35 @@
 ﻿namespace Catan.Domain
 {
-    public sealed record Coordinates(int x, int y);
+    public sealed class Coordinates
+    {
+        private int x { get; set; }
+
+        private int y { get; set; }
+
+        public Coordinates(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int GetX()
+        {
+            return this.x;
+        }
+
+        public int GetY()
+        {
+            return this.y;
+        }
+
+        public static Coordinates Add(Coordinates c1, Coordinates c2)
+        {
+            return new Coordinates(c1.x + c2.x, c1.y + c2.y);
+        }
+
+        public static Coordinates Subtract(Coordinates c1, Coordinates c2)
+        {
+            return new Coordinates(c2.x - c1.x, c2.y - c1.y);
+        }
+    }
 }
