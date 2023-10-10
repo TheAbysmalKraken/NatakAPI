2dd7ead1ccd77d137b7d93d31f679e5e18a8f85b
Jamie Canning <theabysmalkraken@gmail.com>
2023-10-10 20:25:13 +0100

    Create DiceRoller.cs

---
+++ Catan/Catan.Domain/DiceRoller.cs
@@ -0,0 +1,20 @@
+﻿namespace Catan.Domain
+{
+    public static class DiceRoller
+    {
+        private static readonly Random random = new Random();
+
+        public static List<int> RollDice(int numberOfDice, int diceSides)
+        {
+            List<int> rolledDice = new();
+
+            for (var i = 0; i < numberOfDice; i++)
+            {
+                var numberRolled = random.Next(diceSides);
+                rolledDice.Add(numberRolled);
+            }
+
+            return rolledDice;
+        }
+    }
+}
