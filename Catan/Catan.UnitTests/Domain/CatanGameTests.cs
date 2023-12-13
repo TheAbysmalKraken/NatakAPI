7948217d6a7c2985c7cbfeeac0219ffd8e4635c4
Jamie Canning <canningjamie@gmail.com>
2023-12-13 17:52:46 +0000

    Test for development card shuffling

--- Catan/Catan.UnitTests/Domain/CatanGameTests.cs
+++ Catan/Catan.UnitTests/Domain/CatanGameTests.cs
@@ -17,4 +17,21 @@ public class CatanGameTests
         // Assert
         Assert.Equal(numberOfPlayers, players.Count);
     }
+
+    [Fact]
+    public void CreateCatanGame_DevelopmentCardsStartShuffled()
+    {
+        // Arrange
+        var numberOfPlayers = 4;
+
+        // Act
+        var game1 = new CatanGame(numberOfPlayers);
+        var game2 = new CatanGame(numberOfPlayers);
+
+        var game1DevCards = game1.GetRemainingDevelopmentCards();
+        var game2DevCards = game2.GetRemainingDevelopmentCards();
+
+        // Assert
+        Assert.NotEqual(game1DevCards, game2DevCards);
+    }
 }
